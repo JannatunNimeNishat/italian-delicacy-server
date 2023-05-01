8215f45cc717466ba14d010175de929c4b392f10
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-01 22:16:33 +0600

    chefs json and api added

--- index.js
+++ index.js
@@ -2,11 +2,19 @@ const express = require('express')
 const app = express();
 const port = process.env.port || 5000;
 const cors = require('cors')
+
+const chefs = require('./data/chefs.json')
+
 app.use(cors());
+
 app.get('/',(req,res)=>{
     res.send('italian delicacy up and running')
 })
 
+//chefs
+app.get('/chefs',(req,res)=>{
+    res.send(chefs);
+})
 
 app.listen(port,()=>{
     console.log('italian delicacy is running at ',port);
