cdd6cb820505c372715d2d931e627d6e3acdbdc6
JannatunNimeNishat <nishatnime100@gmail.com>
2023-05-01 22:47:37 +0600

    recipes json added and api that filter recipes based on chef's id

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = process.env.port || 5000;
 const cors = require('cors')
 
 const chefs = require('./data/chefs.json')
-
+const recipes = require('./data/recipes.json')
 app.use(cors());
 
 app.get('/',(req,res)=>{
@@ -15,6 +15,14 @@ app.get('/',(req,res)=>{
 app.get('/chefs',(req,res)=>{
     res.send(chefs);
 })
+//recipes
+app.get('/recipes/:chefs_id',(req,res)=>{
+    const chefs_id = req.params.chefs_id;
+    console.log('id:',chefs_id);
+    const chefsRecipes =  recipes.filter(recipe => recipe.chef_id === parseInt(chefs_id));
+    res.send(chefsRecipes);
+})
+
 
 app.listen(port,()=>{
     console.log('italian delicacy is running at ',port);
